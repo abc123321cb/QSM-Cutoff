cee6d0a25c057f1730e8171bbdcdf317756b4368
Yun-Rong Luo <r10943108@ntu.edu.tw>
2024-03-14 10:23:50 -0400

    Clean up prime.py

--- prime.py
+++ prime.py
@@ -5,11 +5,20 @@ from pysat.card import ITotalizer
 from pysat.solvers import Cadical153 as SatSolver 
 from protocol import * 
 
+def make_key(values: List[str], protocol : Protocol) -> str:
+    predicates = []
+    for (atom_id, val) in enumerate(values):
+        if val == '1':
+            predicates.append(protocol.atom_sig[atom_id][0])
+        elif val == '0':
+            predicates.append('~'+protocol.atom_sig[atom_id][0])
+    predicates.sort()
+    return str(predicates)
+
 class Prime():
     # static members
     count  : int = 0 
     _atoms    : List[str]
-    _atom_sig : List[List[str]]
 
     def __init__(self, values: List[str]) -> None:
         self.values : List[str] = values
@@ -24,65 +33,46 @@ class Prime():
             elif val == '0':
                 literals.append('~'+Prime._atoms[atom_id])
         literals.sort()
-        return '{}: {}\n'.format(self.id, str(literals))
-
-    def signature(self) -> str:
-        predicates = []
-        for (atom_id, val) in enumerate(self.values):
-            if val == '1':
-                predicates.append(Prime._atom_sig[atom_id][0])
-            elif val == '0':
-                predicates.append('~'+Prime._atoms[atom_id][0])
-        predicates.sort()
-        return str(predicates)
+        return f'{self.id} : {str(literals)}\n' 
 
     @staticmethod
     def setup(protocol : Protocol) -> None:
         Prime._atoms    = protocol.atoms
-        Prime._atom_sig = protocol.atom_sig
 
 class PrimeOrbit():
     # static members
     count : int = 0
-    _protocol : Protocol
 
-    def __init__(self, prime : Prime) -> None:
-        self.repr_prime : Prime       = prime
+    def __init__(self) -> None:
+        self.repr_prime : Prime      
         self.primes     : List[Prime] = []
-        self.id         : int         = PrimeOrbit.count 
+        self.id         : int = PrimeOrbit.count   
         self.quantified_form  : str = ""
         self.qcost            : int = 0
-
         PrimeOrbit.count += 1
-        self._finalize_orbit()
 
     def __str__(self) -> str:
-        lines  = '= Orbit {} ========================\n'.format(self.id)
-        lines += 'size: {}\n'.format(len(self.primes))
+        lines  = f'= Orbit {self.id} ========================\n'
+        lines += f'size: {len(self.primes)}\n'
         for prime in self.primes:
             lines += str(prime) 
         lines += '\n\n'
         return lines
 
-    def _finalize_orbit(self) -> None:
-        prime_values = self.repr_prime.values
-        permuted_val_list = PrimeOrbit._protocol.permute(prime_values)
-        for values in permuted_val_list:
-            self.primes.append(Prime(values))
-
-    @staticmethod
-    def setup(protocol : Protocol) -> None:
-        PrimeOrbit._protocol = protocol
+    def add_prime(self, prime: Prime):
+        if len(self.primes) == 0:
+            self.repr_prime  = prime
+        self.primes.append(prime)
 
 class DualRailNegation():
     def __init__(self, protocol : Protocol) -> None:
         self.clauses    : List[List[int]] = [] 
-        self.max_var    : int = 0
         self._totalizer : ITotalizer
+        self._atom_num  : int = protocol.atom_num 
         self._atom_id2vars : Dict[int, List[int]] = {}
 
         self._encode(protocol)
-        self._set_totalizer(protocol.atom_num)
+        self._set_totalizer()
 
     def _set_dualrail_vars(self, id: int) -> None:
         self._atom_id2vars[id] = [id*2+2, id*2+1]
@@ -94,13 +84,11 @@ class DualRailNegation():
         return self._atom_id2vars[id][0]
 
     def _encode(self, protocol : Protocol) -> None:
-        for atom_id in range(protocol.atom_num):
+        for atom_id in range(self._atom_num):
             self._set_dualrail_vars(atom_id) 
             pvar = self._get_pos_var(atom_id)
             nvar = self._get_neg_var(atom_id)
             self.clauses.append([-1*pvar, -1*nvar]) # (~pvar + ~nvar)
-
-        self.max_var = 2*(protocol.atom_num)
 
         for state in protocol.reachable_states:
             clause = []
@@ -111,44 +99,41 @@ class DualRailNegation():
                     clause.append(self._get_neg_var(atom_id))
             self.clauses.append(clause)
 
-    def _set_totalizer(self, atom_num : int) -> None:
-        literals = list(range(1,self.max_var+1))
-        self._totalizer = ITotalizer(lits=literals, ubound=atom_num, top_id=self.max_var)
+    def _set_totalizer(self) -> None:
+        max_var = 2 * self._atom_num
+        literals = list(range(1,max_var+1))
+        self._totalizer = ITotalizer(lits=literals, ubound=self._atom_num, top_id=max_var)
         self.clauses.extend(self._totalizer.cnf.clauses)
 
     def assume(self, ubound: int) -> List[int]:
         return [-1*self._totalizer.rhs[ubound]]
 
-    def get_single_rail(self, sat_model, atom_num : int) -> Prime:
-        values = ['-']*atom_num
-        for atom_id in range(atom_num):
+    def block(self, values : List[str]) -> List[int]:
+        var = 0
+        block_clause = []
+        for (atom_id, value) in enumerate(values):
+            if value == '1':
+                var = self._get_pos_var(atom_id)
+                block_clause.append(-1*var)
+            elif value == '0':
+                var = self._get_neg_var(atom_id)
+                block_clause.append(-1*var)
+        return block_clause
+
+    def single_rail(self, sat_model) -> List[str]:
+        values = ['-']* self._atom_num
+        for atom_id in range(self._atom_num):
             if self._get_pos_var(atom_id) in sat_model:
                 values[atom_id] = '1'
             elif self._get_neg_var(atom_id) in sat_model:
                 values[atom_id] = '0'
-        return Prime(values)
-
-    def block(self, orbit: PrimeOrbit) -> List[List[int]]:
-        var = 0
-        block_clauses = []
-        for prime in orbit.primes:
-            values = prime.values
-            block_clause = []
-            for (atom_id, value) in enumerate(values):
-                if value == '1':
-                    var = self._get_pos_var(atom_id)
-                    block_clause.append(-1*var)
-                elif value == '0':
-                    var = self._get_neg_var(atom_id)
-                    block_clause.append(-1*var)
-            block_clauses.append(block_clause)
-        return block_clauses
+        return values
 
 class PrimeOrbits():
     def __init__(self) -> None:
-        self.orbits     : List[PrimeOrbit] = [] 
-        self._formula   : DualRailNegation
-        self._sig2Orbit : Dict[str, PrimeOrbit]
+        self.orbits      : List[PrimeOrbit] = [] 
+        self._formula    : DualRailNegation
+        self._orbit_hash : Dict[str, PrimeOrbit] = {}
 
     def __str__(self) -> str:
         lines ='' 
@@ -156,24 +141,34 @@ class PrimeOrbits():
             lines += str(orbit) 
         return lines
 
-    def add_orbit(self, prime: Prime) -> PrimeOrbit:
-
+    def _make_orbit(self, values: List[str], protocol : Protocol) -> List[List[int]]:
+        key = make_key(values,protocol)
+        if not key in self._orbit_hash:
+            orbit = PrimeOrbit()
+            self._orbit_hash[key] = orbit
+            self.orbits.append(orbit)
+        orbit = self._orbit_hash[key]
+        block_clauses = []
+        for new_values in protocol.permute(values):
+            clause = self._formula.block(new_values) 
+            block_clauses.append(clause)
+            prime  = Prime(new_values)
+            orbit.add_prime(prime)
+        return block_clauses
 
     def symmetry_aware_enumerate(self, protocol: Protocol) -> None:
         Prime.setup(protocol)
-        PrimeOrbit.setup(protocol)
         atom_num = protocol.atom_num
 
         self._formula = DualRailNegation(protocol)
-        # ub=0 will cover the case when the formula is empty ????
+        # ub=0 will cover the case when the formula is empty 
         with SatSolver(bootstrap_with=self._formula.clauses) as sat_solver:
             for ubound in range(0,atom_num+1):
                 assumptions = self._formula.assume(ubound)
                 result = sat_solver.solve(assumptions)
                 while (result):
-                    model = sat_solver.get_model()
-                    prime = self._formula.get_single_rail(model, atom_num) 
-                    orbit = self.add_orbit(prime)                    
-                    block_clauses = self._formula.block(orbit)
-                    sat_solver.append_formula(block_clauses)
+                    model  = sat_solver.get_model()
+                    values = self._formula.single_rail(model)
+                    block_clauses  = self._make_orbit(values, protocol)
+                    sat_solver.append_formula(block_clauses) 
                     result = sat_solver.solve(assumptions)
