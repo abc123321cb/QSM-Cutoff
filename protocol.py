451939731dac50a9e9d5ce10f2ecdee61ca686ca
Yun-Rong Luo <r10943108@ntu.edu.tw>
2024-07-12 14:42:40 -0400

    minor update protocol.py: verbose print symmetric group order

--- protocol.py
+++ protocol.py
@@ -4,6 +4,7 @@ from itertools import permutations, product
 from transition import TransitionSystem
 from util import QrmOptions, SET_DELIM
 from verbose import *
+from math import factorial as fact
 
 # utils
 set_delim = SET_DELIM
@@ -239,9 +240,13 @@ class Protocol():
         outF.close()
 
     def print_verbose(self) -> None:
+        sym_group_order = 1
+        for constants in self.sort_constants:
+            sym_group_order *= fact(len(constants))
         vprint_step_banner(self.options, f'[FW RESULT]: Forward Reachability on [{self.options.instance_name}: {self.options.size_str}]', 3)
         vprint(self.options, '\n'.join(self.lines), 3)
         vprint(self.options, f'[FW NOTE]: number of variables: {self.atom_num}', 2)
+        vprint(self.options, f'[FW NOTE]: symmetric group order: {sym_group_order}', 2)
     
     def _get_renamed_arguments(self, permutation, sort_id, arguments) -> str:
         new_constant = []
