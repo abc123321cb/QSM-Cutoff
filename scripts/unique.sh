d9c5b8cf2fe680b387a298f9f9cd79311650dcc3
Yun-Rong Luo <r10943108@ntu.edu.tw>
2025-01-13 13:08:35 -0500

    add unique.sh

---
+++ scripts/unique.sh
@@ -0,0 +1,54 @@
+sym_path='ivybench/sym/ivy/'
+sym_quorum_path='ivybench/sym_quorum/ivy/'
+sym_ring_path='ivybench/sym_ring/ivy/'
+yaml_path='yamls/'
+Timeout=36000
+
+declare -a sym_instances=(
+    "Consensus"
+    "TCommit"
+    "Ricart-Agrawala"
+    "lock_server"
+    "sharded_kv"
+    "sharded_kv_no_lost_keys"
+    "simple-decentralized-lock"
+    "firewall"
+    "lockserv"
+    "lockserv_automaton"
+    "client_server_ae"
+    "TwoPhase"
+)
+
+declare -a sym_quorum_instances=(
+    "toy_consensus"
+    "toy_consensus_epr"
+    "naive_consensus"
+    "simple-election"
+    "consensus_epr"
+    "toy_consensus_forall"
+#    "quorum-leader-election-wo-maj" need -m
+)
+
+declare -a hard_sym_instances=(
+    "learning_switch_forallexists"
+    "client_server_db_ae"
+    "learning_switch"
+    "two_phase_commit"
+)
+
+declare -a hard_sym_quorum_instances=(
+    "consensus_forall"
+    "consensus_wo_decide"
+)
+
+declare -a hard_sym_ring_instances=(
+    "Simple"
+    "SimpleRegular"
+    "chord_ring_maintenance"
+)
+
+timeout ${Timeout} python3 qrm.py ${sym_path}sharded_kv_no_lost_keys.ivy              -s node=2,key=1,value=1     -v 5 -w -l ${sym_path}sharded_kv_no_lost_keys.unique.log 
+timeout ${Timeout} python3 qrm.py ${sym_quorum_path}toy_consensus.ivy                 -s node=3,value=1           -v 5 -w -l ${sym_quorum_path}toy_consensus.unique.log
+timeout ${Timeout} python3 qrm.py ${sym_quorum_path}toy_consensus_epr.ivy             -s node=3,value=1           -v 5 -w -l ${sym_quorum_path}toy_consensus_epr.unique.log 
+timeout ${Timeout} python3 qrm.py ${sym_quorum_path}toy_consensus_forall.ivy          -s node=5,value=2           -v 5 -w -l ${sym_quorum_path}toy_consensus_forall.unique.log 
+timeout ${Timeout} python3 qrm.py ${sym_quorum_path}quorum-leader-election-wo-maj.ivy -s node=5                   -v 5 -w -l ${sym_quorum_path}quorum-leader-election-wo-maj.unique.log
