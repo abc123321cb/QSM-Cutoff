f246907c2b678ce76d6f85018503783dcd423fed
Yun-Rong L Luo <yunrong@caen-vnc-mi17.engin.umich.edu>
2025-01-20 21:27:50 -0500

    Update cutoff results

--- logs/cutoff/misc/TwoPhase.resource_manager_1.finite.cpp
+++ logs/cutoff/misc/TwoPhase.resource_manager_1.finite.cpp
@@ -704,44 +704,14 @@ void TwoPhase__resource_manager_1__finite::ext__rMRcvAbortMsg(resource_manager r
         committed[rm] = false;
         aborted[rm] = true;
 }
-bool TwoPhase__resource_manager_1__finite::ext__get_tm_prepared(resource_manager r0){
+bool TwoPhase__resource_manager_1__finite::ext__get_tm_aborted(){
     bool qrm_result;
-    qrm_result = tm_prepared[r0];
+    qrm_result = tm_aborted;
     return qrm_result;
 }
-bool TwoPhase__resource_manager_1__finite::ext__get_bool_tm_prepared(resource_manager r0, bool result){
+bool TwoPhase__resource_manager_1__finite::ext__get_bool_tm_aborted(bool result){
     bool qrm_result;
-    qrm_result = (tm_prepared[r0] == result);
-    return qrm_result;
-}
-bool TwoPhase__resource_manager_1__finite::ext__get_tm_init(){
-    bool qrm_result;
-    qrm_result = tm_init;
-    return qrm_result;
-}
-bool TwoPhase__resource_manager_1__finite::ext__get_bool_tm_init(bool result){
-    bool qrm_result;
-    qrm_result = (tm_init == result);
-    return qrm_result;
-}
-bool TwoPhase__resource_manager_1__finite::ext__get_committed(resource_manager r0){
-    bool qrm_result;
-    qrm_result = committed[r0];
-    return qrm_result;
-}
-bool TwoPhase__resource_manager_1__finite::ext__get_bool_committed(resource_manager r0, bool result){
-    bool qrm_result;
-    qrm_result = (committed[r0] == result);
-    return qrm_result;
-}
-bool TwoPhase__resource_manager_1__finite::ext__get_msg_prepared(resource_manager r0){
-    bool qrm_result;
-    qrm_result = msg_prepared[r0];
-    return qrm_result;
-}
-bool TwoPhase__resource_manager_1__finite::ext__get_bool_msg_prepared(resource_manager r0, bool result){
-    bool qrm_result;
-    qrm_result = (msg_prepared[r0] == result);
+    qrm_result = (tm_aborted == result);
     return qrm_result;
 }
 bool TwoPhase__resource_manager_1__finite::ext__get_working(resource_manager r0){
@@ -754,14 +724,14 @@ bool TwoPhase__resource_manager_1__finite::ext__get_bool_working(resource_manage
     qrm_result = (working[r0] == result);
     return qrm_result;
 }
-bool TwoPhase__resource_manager_1__finite::ext__get_tm_aborted(){
+bool TwoPhase__resource_manager_1__finite::ext__get_aborted(resource_manager r0){
     bool qrm_result;
-    qrm_result = tm_aborted;
+    qrm_result = aborted[r0];
     return qrm_result;
 }
-bool TwoPhase__resource_manager_1__finite::ext__get_bool_tm_aborted(bool result){
+bool TwoPhase__resource_manager_1__finite::ext__get_bool_aborted(resource_manager r0, bool result){
     bool qrm_result;
-    qrm_result = (tm_aborted == result);
+    qrm_result = (aborted[r0] == result);
     return qrm_result;
 }
 bool TwoPhase__resource_manager_1__finite::ext__get_msg_commit(){
@@ -774,24 +744,14 @@ bool TwoPhase__resource_manager_1__finite::ext__get_bool_msg_commit(bool result)
     qrm_result = (msg_commit == result);
     return qrm_result;
 }
-bool TwoPhase__resource_manager_1__finite::ext__get_tm_committed(){
+bool TwoPhase__resource_manager_1__finite::ext__get_tm_init(){
     bool qrm_result;
-    qrm_result = tm_committed;
+    qrm_result = tm_init;
     return qrm_result;
 }
-bool TwoPhase__resource_manager_1__finite::ext__get_bool_tm_committed(bool result){
+bool TwoPhase__resource_manager_1__finite::ext__get_bool_tm_init(bool result){
     bool qrm_result;
-    qrm_result = (tm_committed == result);
-    return qrm_result;
-}
-bool TwoPhase__resource_manager_1__finite::ext__get_aborted(resource_manager r0){
-    bool qrm_result;
-    qrm_result = aborted[r0];
-    return qrm_result;
-}
-bool TwoPhase__resource_manager_1__finite::ext__get_bool_aborted(resource_manager r0, bool result){
-    bool qrm_result;
-    qrm_result = (aborted[r0] == result);
+    qrm_result = (tm_init == result);
     return qrm_result;
 }
 bool TwoPhase__resource_manager_1__finite::ext__get_msg_abort(){
@@ -804,6 +764,26 @@ bool TwoPhase__resource_manager_1__finite::ext__get_bool_msg_abort(bool result){
     qrm_result = (msg_abort == result);
     return qrm_result;
 }
+bool TwoPhase__resource_manager_1__finite::ext__get_committed(resource_manager r0){
+    bool qrm_result;
+    qrm_result = committed[r0];
+    return qrm_result;
+}
+bool TwoPhase__resource_manager_1__finite::ext__get_bool_committed(resource_manager r0, bool result){
+    bool qrm_result;
+    qrm_result = (committed[r0] == result);
+    return qrm_result;
+}
+bool TwoPhase__resource_manager_1__finite::ext__get_tm_committed(){
+    bool qrm_result;
+    qrm_result = tm_committed;
+    return qrm_result;
+}
+bool TwoPhase__resource_manager_1__finite::ext__get_bool_tm_committed(bool result){
+    bool qrm_result;
+    qrm_result = (tm_committed == result);
+    return qrm_result;
+}
 bool TwoPhase__resource_manager_1__finite::ext__get_prepared(resource_manager r0){
     bool qrm_result;
     qrm_result = prepared[r0];
@@ -812,6 +792,26 @@ bool TwoPhase__resource_manager_1__finite::ext__get_prepared(resource_manager r0
 bool TwoPhase__resource_manager_1__finite::ext__get_bool_prepared(resource_manager r0, bool result){
     bool qrm_result;
     qrm_result = (prepared[r0] == result);
+    return qrm_result;
+}
+bool TwoPhase__resource_manager_1__finite::ext__get_msg_prepared(resource_manager r0){
+    bool qrm_result;
+    qrm_result = msg_prepared[r0];
+    return qrm_result;
+}
+bool TwoPhase__resource_manager_1__finite::ext__get_bool_msg_prepared(resource_manager r0, bool result){
+    bool qrm_result;
+    qrm_result = (msg_prepared[r0] == result);
+    return qrm_result;
+}
+bool TwoPhase__resource_manager_1__finite::ext__get_tm_prepared(resource_manager r0){
+    bool qrm_result;
+    qrm_result = tm_prepared[r0];
+    return qrm_result;
+}
+bool TwoPhase__resource_manager_1__finite::ext__get_bool_tm_prepared(resource_manager r0, bool result){
+    bool qrm_result;
+    qrm_result = (tm_prepared[r0] == result);
     return qrm_result;
 }
 void TwoPhase__resource_manager_1__finite::__tick(int __timeout){
--- logs/cutoff/misc/toy_consensus_forall.node_3_value_3.finite.cpp
+++ logs/cutoff/misc/toy_consensus_forall.node_3_value_3.finite.cpp
@@ -628,14 +628,14 @@ void toy_consensus_forall__node_3_value_3__finite::__init(){
             voted[N] = __tmp0[N];
         }
         bool __tmp1[3][3];
-        for (int V = 0; V < 3; V++) {
-            for (int N = 0; N < 3; N++) {
-                __tmp1[V][N] = false;
+        for (int N = 0; N < 3; N++) {
+            for (int V = 0; V < 3; V++) {
+                __tmp1[N][V] = false;
             }
         }
-        for (int V = 0; V < 3; V++) {
-            for (int N = 0; N < 3; N++) {
-                vote[N][V] = __tmp1[V][N];
+        for (int N = 0; N < 3; N++) {
+            for (int V = 0; V < 3; V++) {
+                vote[N][V] = __tmp1[N][V];
             }
         }
         bool __tmp2[3];
@@ -676,6 +676,16 @@ void toy_consensus_forall__node_3_value_3__finite::ext__decide(value v, quorum q
         decided[v] = true;
         voting_quorum = q;
 }
+bool toy_consensus_forall__node_3_value_3__finite::ext__get_decided(value v0){
+    bool qrm_result;
+    qrm_result = decided[v0];
+    return qrm_result;
+}
+bool toy_consensus_forall__node_3_value_3__finite::ext__get_bool_decided(value v0, bool result){
+    bool qrm_result;
+    qrm_result = (decided[v0] == result);
+    return qrm_result;
+}
 toy_consensus_forall__node_3_value_3__finite::quorum toy_consensus_forall__node_3_value_3__finite::ext__get_voting_quorum(){
     toy_consensus_forall__node_3_value_3__finite::quorum qrm_result;
     qrm_result = voting_quorum;
@@ -684,16 +694,6 @@ toy_consensus_forall__node_3_value_3__finite::quorum toy_consensus_forall__node_
 bool toy_consensus_forall__node_3_value_3__finite::ext__get_bool_voting_quorum(quorum result){
     bool qrm_result;
     qrm_result = (voting_quorum == result);
-    return qrm_result;
-}
-bool toy_consensus_forall__node_3_value_3__finite::ext__get_member(node n0, quorum q1){
-    bool qrm_result;
-    qrm_result = member[n0][q1];
-    return qrm_result;
-}
-bool toy_consensus_forall__node_3_value_3__finite::ext__get_bool_member(node n0, quorum q1, bool result){
-    bool qrm_result;
-    qrm_result = (member[n0][q1] == result);
     return qrm_result;
 }
 bool toy_consensus_forall__node_3_value_3__finite::ext__get_voted(node n0){
@@ -706,6 +706,16 @@ bool toy_consensus_forall__node_3_value_3__finite::ext__get_bool_voted(node n0, 
     qrm_result = (voted[n0] == result);
     return qrm_result;
 }
+bool toy_consensus_forall__node_3_value_3__finite::ext__get_member(node n0, quorum q1){
+    bool qrm_result;
+    qrm_result = member[n0][q1];
+    return qrm_result;
+}
+bool toy_consensus_forall__node_3_value_3__finite::ext__get_bool_member(node n0, quorum q1, bool result){
+    bool qrm_result;
+    qrm_result = (member[n0][q1] == result);
+    return qrm_result;
+}
 bool toy_consensus_forall__node_3_value_3__finite::ext__get_vote(node n0, value v1){
     bool qrm_result;
     qrm_result = vote[n0][v1];
@@ -714,16 +724,6 @@ bool toy_consensus_forall__node_3_value_3__finite::ext__get_vote(node n0, value 
 bool toy_consensus_forall__node_3_value_3__finite::ext__get_bool_vote(node n0, value v1, bool result){
     bool qrm_result;
     qrm_result = (vote[n0][v1] == result);
-    return qrm_result;
-}
-bool toy_consensus_forall__node_3_value_3__finite::ext__get_decided(value v0){
-    bool qrm_result;
-    qrm_result = decided[v0];
-    return qrm_result;
-}
-bool toy_consensus_forall__node_3_value_3__finite::ext__get_bool_decided(value v0, bool result){
-    bool qrm_result;
-    qrm_result = (decided[v0] == result);
     return qrm_result;
 }
 void toy_consensus_forall__node_3_value_3__finite::__tick(int __timeout){
