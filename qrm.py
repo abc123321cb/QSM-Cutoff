918a98db1553ebf2b7e347ee4e6f1bf9b020a133
Jonah Kelman <jonahkel@umich.edu>
2026-02-12 18:51:36 -0500

    Updated distributed_lock.ivy in total/. Removed reduction step.

--- qrm.py
+++ qrm.py
@@ -219,7 +219,7 @@ def qrm(ivy_name, args):
     # reduction
     options.step_start(f'[RED]: PRIME REDUCTION on [{options.ivy_filename}: {options.size_str}]')
     minimizer    = Minimizer(options, tran_sys, instantiator, prime_orbits.orbits, curried_protocol.more_reach)
-    minimizer.reduce_redundant_prime_orbits()
+    # minimizer.reduce_redundant_prime_orbits()
     options.step_end()
 
     # quantifier inference
